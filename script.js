8a0e2e22b541dd766452014fba1a0a2078f662c1
in-sprt <1787.db@gmail.com>
2024-09-16 00:38:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+lightbox.option({
+    'resizeDuration': 200,  //  Время  анимации  изменения  размера  (мс)
+    'wrapAround': true,  //  Зацикливание  галереи
+    'albumLabel': "%1 из %2",  //  Формат  метки  "Номер  изображения  из  общего  количества"
+    //  ...  другие  опции
+}); 
 // Плавный скролл при клике на ссылки в меню
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -8,12 +14,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-
-// Lightbox для портфолио (опционально)
-// ... 
-lightbox.option({
-    'resizeDuration': 200,  //  Время  анимации  изменения  размера  (мс)
-    'wrapAround': true,  //  Зацикливание  галереи
-    'albumLabel': "%1 из %2",  //  Формат  метки  "Номер  изображения  из  общего  количества"
-    //  ...  другие  опции
-}); 
