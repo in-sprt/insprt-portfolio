53e354941960e6584ad21c3c0d43c07d809a6908
in-sprt <1787.db@gmail.com>
2024-09-16 00:57:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-// Плавный скролл при клике на ссылки в меню jr
+// Плавный скролл при клике на ссылки в меню
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
 
-lightbox.option({
-    'resizeDuration': 200,  //  Время  анимации  изменения  размера  (мс)
-    'wrapAround': true,  //  Зацикливание  галереи
-    'albumLabel': "%1 из %2",  //  Формат  метки  "Номер  изображения  из  общего  количества"
-    //  ...  другие  опции
-}); 
+// Инициализация Lightbox (после загрузки DOM)
+document.addEventListener('DOMContentLoaded', function() {
+  lightbox.option({
+    'resizeDuration': 200, // Время анимации изменения размера (мс)
+    'wrapAround': true,   // Зацикливание галереи
+    'albumLabel': "%1 из %2", // Формат метки "Номер изображения из общего количества"
+    // ... другие опции
+  });
+});
+
+// Навбар (полупрозрачность при прокрутке)
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
