958a5f47331d636c3e119c1466d639a1d70a4b5e
in-sprt <1787.db@gmail.com>
2024-09-24 11:08:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,37 +18,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-/* эффект паралакса */
+///  эффект  паралакса
 window.addEventListener('scroll', () => {
   const parallaxBg = document.querySelector('.parallax-bg');
   let scrollY = window.pageYOffset;
 
-  parallaxBg.style.backgroundPositionY = scrollY * 0.5 + 'px'; //  На  ваше  усмотрение  скорость  
-});
-
-/* Фильтр портфолио */
-
-const filterButtons = document.querySelectorAll('.filter-button');
-const portfolioItems = document.querySelectorAll('.portfolio-item');
-
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const category = button.dataset.filter;
-
-    portfolioItems.forEach(item => {
-      if (category === 'all' || item.dataset.category === category) {
-        item.style.display = 'block';
-      } else {
-        item.style.display = 'none';
-      }
-    });
-  });
-});
-
-/* Fancy box */
-
-Fancybox.bind("[data-fancybox='gallery']", {
-  //  Опции  Fancybox
+  parallaxBg.style.backgroundPositionY = scrollY * 0.5 + 'px'; 
 });
 
 /* кнопка "Подробнее" */
@@ -70,4 +45,33 @@ serviceItems.forEach(item => {
     description.style.display = description.style.display === 'none' ? 'block' : 'none';
     button.textContent = button.textContent === 'Подробнее' ? 'Скрыть' : 'Подробнее';
   });
+});
+
+// Фильтр портфолио 
+
+const filterButtons = document.querySelectorAll('.filter-button');
+const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const category = button.dataset.filter;
+
+    portfolioItems.forEach(item => {
+      if (category === 'all' || item.dataset.category === category) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+
+    //  Добавляем  класс  "active"  к  нажатой  кнопке
+    filterButtons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+/* Fancy box */
+
+Fancybox.bind("[data-fancybox='gallery']", {
+  //  Опции  Fancybox
 });
